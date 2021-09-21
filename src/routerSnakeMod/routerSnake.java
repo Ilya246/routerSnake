70194479c6e7c50131709d57db5bd563c228f751
Ilya246 <57039557+Ilya246@users.noreply.github.com>
2021-09-21 19:05:46 +0400

    Add files via upload

--- src/routerSnakeMod/routerSnake.java
+++ src/routerSnakeMod/routerSnake.java
@@ -40,7 +40,7 @@ public class routerSnake{
 
     public void update(){
         i += Time.delta;
-        if(i > 10f){
+        if(i > routerSnakeMod.snakeUpdateDelay){
             if(length < 1){
                 routerSnakeMod.snakes.remove(this);
                 return;
@@ -63,63 +63,62 @@ public class routerSnake{
             };
             if(y > routerSnakeMod.worldHeight){
                 heading = 270f;
-            }else if(y < 0){
+            }else if(y < 0f){
                 heading = 90f;
             };
-            heading += Mathf.random(-18f, 18f);
-            x += Mathf.cosDeg(heading) * 10f;
-            y += Mathf.sinDeg(heading) * 10f;
-            if(Mathf.chance(0.008f) && Groups.player.size() > 0){
+            heading += Mathf.random(-routerSnakeMod.snakeHeadingRnd, routerSnakeMod.snakeHeadingRnd);
+            x += Mathf.cosDeg(heading) * routerSnakeMod.snakeSegLength;
+            y += Mathf.sinDeg(heading) * routerSnakeMod.snakeSegLength;
+            if(Mathf.chance(routerSnakeMod.snakeLockOnChc) && Groups.player.size() > 0){
                 target = Groups.player.index(Mathf.random(Groups.player.size() - 1));
-            } else if(Mathf.chance(0.008f)){
+            }else if(Mathf.chance(routerSnakeMod.snakeLockOffChc)){
                 target = null;
             };
-            if((length > 12 || canDecay) && Mathf.chance(0.0005f * Math.max((float)length, 10))){
+            if((length > routerSnakeMod.minPermSnakeSize || canDecay) && Mathf.chance(routerSnakeMod.snakeDecayRate * Math.max((float)length, routerSnakeMod.minPermSnakeSize))){
                 length--;
                 segments.remove(0);
             };
-            if(target != null && target.unit().type != null){
-                heading = Mathf.slerp(heading, Mathf.angle(target.x - x, target.y - y), 0.25f);
-                if(Mathf.len(target.x - x, target.y - y) < 80f){
-                    target.unit().heal((target.unit().maxHealth / 1000f + 10f) * (float)length / 12f);
+            if(target != null && !target.unit().isNull()){
+                heading = Mathf.slerp(heading, Mathf.angle(target.x - x, target.y - y), routerSnakeMod.snakeChaseRate);
+                if(Mathf.len(target.x - x, target.y - y) < routerSnakeMod.snakeHealRange){
+                    target.unit().heal((target.unit().maxHealth / 10000f + 1f) * (float)length * routerSnakeMod.snakeUnHealRate);
                 };
             };
             segments.each(s -> {
                 float segX = s[0];
                 float segY = s[1];
-                Tile newTile = world.tile(Mathf.floor(segX / 8), Mathf.floor(segY / 8));
+                Tile newTile = world.tile(Mathf.floor(segX / tilesize), Mathf.floor(segY / tilesize));
                 if(newTile != null && newTile.build != null){
                     Building newBuild = newTile.build;
                     if(newBuild.block != Blocks.distributor){
-                        newBuild.heal(1f / 100f * newBuild.maxHealth());
+                        newBuild.heal(routerSnakeMod.snakeBlHealRate / 100f * newBuild.maxHealth());
                     };
                 };
             });
-            Tile newTile = world.tile(Mathf.floor(x / 8), Mathf.floor(y / 8));
+            Tile newTile = world.tile(Mathf.floor(x / tilesize), Mathf.floor(y / tilesize));
             if(newTile != null && newTile.build != null){
                 Building newBuild = newTile.build;
                 if(newBuild.block == Blocks.distributor){
-                    newBuild.damage(Math.min(40f * (float)length / 12f, 79f));
-                    x -= Mathf.cosDeg(heading) * 10f;
-                    y -= Mathf.sinDeg(heading) * 10f;
+                    newBuild.damage(Math.min(routerSnakeMod.baseDistributorDmg * (float)length / routerSnakeMod.minPermSnakeSize, routerSnakeMod.maxDistributorDmg));
+                    x -= Mathf.cosDeg(heading) * routerSnakeMod.snakeSegLength;
+                    y -= Mathf.sinDeg(heading) * routerSnakeMod.snakeSegLength;
                     heading = Mathf.angle(x - newBuild.x, y - newBuild.y);
-                    if(Mathf.chance(0.02f * length)){
+                    if(Mathf.chance(routerSnakeMod.distributorGrowChc * length)){
                         length++;
                         segments.add(new float[]{x, y});
                     };
-                }else if(newBuild.block == Blocks.router && Mathf.chance((length < 10 && length != 0) ? 0.5f / length : 0.05f)){
-                    if(Mathf.chance(0.01f * length)){
+                }else if(newBuild.block == Blocks.router && Mathf.chance(routerSnakeMod.baseConsumeChc * Math.max(routerSnakeMod.minPermSnakeSize / length, 1f))){
+                    if(Mathf.chance(routerSnakeMod.baseSplitChc * length)){
                         routerSnakeMod.snakes.add(new routerSnake(x, y, true, length / 2));
-                        length = canDecay ? length / 2 : Math.max(length / 2, 12);
+                        length = canDecay ? length / 2 : Math.max(length / 2, routerSnakeMod.minPermSnakeSize);
                         while(segments.size > length){
                             segments.remove(0);
                         };
-                        newBuild.damage(19);
                     }else{
                         length++;
                         segments.add(new float[]{x, y});
-                        newBuild.damage(19);
                     };
+                    newBuild.damage(routerSnakeMod.baseRouterDmg);
                 };
             };
             i -= 10f;
--- src/routerSnakeMod/routerSnakeMod.java
+++ src/routerSnakeMod/routerSnakeMod.java
@@ -24,16 +24,39 @@ public class routerSnakeMod extends Plugin{
     public static String rout = Character.toString(Iconc.blockRouter);
     public static Seq<routerSnake> snakes = new Seq<>();
 
+    // snake vars
+    public static float snakeUpdateDelay = 10f;
+    public static float snakeHeadingRnd = 18f;
+    public static float snakeLockOnChc = 0.008f;
+    public static float snakeLockOffChc = 0.008f;
+    public static int minPermSnakeSize = 12;
+    public static float snakeSegLength = 10f;
+    public static float snakeDecayRate = 0.0005f;
+    public static float snakeChaseRate = 0.25f;
+    public static float snakeHealRange = 80f;
+    public static float snakeUnHealRate = 1f;
+    public static float snakeBlHealRate = 1f;
+    public static float baseDistributorDmg = 40f;
+    public static float maxDistributorDmg = 79f;
+    public static float distributorGrowChc = 0.02f;
+    public static float baseRouterDmg = 19f;
+    public static float baseConsumeChc = 0.05f;
+    public static float baseSplitChc = 0.01f;
+
+    // general vars
+    public static float snakeCountDiv = 2000f;
+    private static int maxSpawnedLength = 100;
+
     @Override
     public void init(){
         Log.info("Router Snake has awoken!");
         Events.on(WorldLoadEvent.class, e -> {
             snakes.clear();
-            worldHeight = world.height() * 8;
-            worldWidth = world.width() * 8;
+            worldHeight = world.height() * tilesize;
+            worldWidth = world.width() * tilesize;
             float snakeScl = Mathf.len(worldWidth, worldHeight);
-            for(int i = 0; i < snakeScl; i += 2000){
-                snakes.add(new routerSnake(0f, 0f, false, 12));
+            for(float i = 0f; i < snakeScl; i += snakeCountDiv){
+                snakes.add(new routerSnake(0f, 0f, false, minPermSnakeSize));
             };
             snakes.each(s -> {
                 s.x = Mathf.random(worldWidth);
@@ -47,12 +70,12 @@ public class routerSnakeMod extends Plugin{
     public void registerClientCommands(CommandHandler handler){
         handler.<Player>register("spawnsnake", "<x> <y> <canDie> <length>", "Spawn a router snake.", (args, player) -> {
             try{
-                if(Integer.parseInt(args[3]) > 100 || Integer.parseInt(args[3]) < 1){
+                if(Integer.parseInt(args[3]) > maxSpawnedLength || Integer.parseInt(args[3]) < 1){
                     player.sendMessage("The specified length is invalid.");
                 }else if(!player.admin){
                     player.sendMessage("You need to be admin to spawn snakes.");
                 }else{
-                    snakes.add(new routerSnake(Float.parseFloat(args[0]) * 8f, Float.parseFloat(args[1]) * 8f, Boolean.parseBoolean(args[2]), Integer.parseInt(args[3])));
+                    snakes.add(new routerSnake(Float.parseFloat(args[0]) * tilesize, Float.parseFloat(args[1]) * tilesize, Boolean.parseBoolean(args[2]), Integer.parseInt(args[3])));
                 };
             }catch(Exception badArguments){
                 player.sendMessage("Invalid arguments.");
